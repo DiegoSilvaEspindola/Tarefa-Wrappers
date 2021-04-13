3722124d181695e15bb8df12f6e30bf23eb44b9c
Diego <diegosilvaespindola@gmail.com>
2021-04-13 16:38:38 -0300

    tarefa comitada

---
+++ src/tarefaWrapper/ConvertWrapper.java
@@ -0,0 +1,13 @@
+package tarefaWrapper;
+
+public class ConvertWrapper {
+
+    public static void main(String args []){
+
+        String meuInteiro = String.valueOf(20);
+
+        System.out.println(meuInteiro);
+
+    }
+
+}
